c393922e105f149e7123b0fc7bfc52c7010a1af8
Emir Akar <emirakar@gmail.com>
2025-03-02 14:02:04 +0300

    Added two methods to Player.java

--- Player.java
+++ Player.java
@@ -13,7 +13,13 @@ public class Player {
      * TODO: removes and returns the tile in given index
      */
     public Tile getAndRemoveTile(int index) {
-        return null;
+        Tile givenTile = this.playerTiles[index];
+        for(int i = index; i < playerTiles.length - 1; i++){
+            playerTiles[i] = playerTiles[i+1];
+        }
+        playerTiles[numberOfTiles - 1] = null;
+        numberOfTiles--;
+        return givenTile;
     }
 
     /*
@@ -37,7 +43,24 @@ public class Player {
      * @return
      */
     public boolean isWinningHand() {
-        return false;
+        int count = 0;
+        int currentChain = 1;
+
+        if (numberOfTiles < 12) return false;
+
+        for(int i = 0; i < playerTiles.length; i++){
+            if ( playerTiles[i] != null && playerTiles[i+1] != null && playerTiles[i].getValue() == playerTiles[i+1].getValue() && playerTiles[i].getColor() != playerTiles[i+1].getColor()) {
+                currentChain++;
+            }
+            else currentChain = 1;
+            
+            if (currentChain == 4) {
+                count++;
+                currentChain = 1;
+            }    
+        }
+
+        return count == 3;
     }
 
     public int findPositionOfTile(Tile t) {
